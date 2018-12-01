9fde42397267f171726f54228a307dc4358341af
mateoromeoo <23mateoromero@gmail.com>
2018-12-01 18:01:33 -0500

    config intranet2

--- intranet/common/config.php
+++ intranet/common/config.php
@@ -2,10 +2,10 @@
 
 // DATOS DE CONEXIÓN A BD
 define('HOST', 'us-cdbr-iron-east-01.cleardb.net');
+define('DATA_BASE', 'heroku_44f5b98f341b91a');
 // define('DATA_BASE', 'tutransporte_bd');
 // define('USER_DB', 'root');
 // define('PASS_DB', '');
-define('DATA_BASE', 'heroku_44f5b98f341b91a');
 define('USER_DB', 'bc001144c9129b');
 define('PASS_DB', '14e28433');
 // fin
@@ -20,22 +20,22 @@ define("MAIL_SECURE","ssl");
 define("MAIL_HOST","mail.beerlandfactory.com");
 define("MAIL_PORT","465");
 define("MAIL_USER","paginaweb@beerlandfactory.com");
-define("MAIL_USER_PASS","hdc2016%");
-define("MAIL_ALTBODY","Mensaje de Intranet - Corapsac");
+define("MAIL_USER_PASS","");
+define("MAIL_ALTBODY","");
 
 //FIN
 
 // DATOS DE EMPRESA QUE DESARROLLA
 define('EMP_NAME', 'Tu Transporte');
-define('EMP_SLOGAN', 'Chocolate de la Amazonía Peruana');
-define('EMP_FB','https://www.facebook.com/hdcperu/?fref=ts');
-define('EMP_WEB','https://agenciahdc.com/');
+define('EMP_SLOGAN', 'Tu web de Transporte favorita');
+define('EMP_FB','');
+define('EMP_WEB','');
 // fin
 
 // ANIMACIÓN INICIAL
 define('BACKGROUND_GO', '#004d3e');
 define('EMP_NAME_GO', '<b>Tu Transporte</b>');
-define('EMP_SLOGAN_GO', 'Chocolate de la Amazonía Peruana');
+define('EMP_SLOGAN_GO', 'Tu web de Transporte favorita');
 define('BACKGROUND_END', '#fff');
 define('COLOR_GO', '#fff');
 define('COLOR_END', '#004d3e');
